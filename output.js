967590346876a5da80a69f75e7d94787e509c651
action <action@github.com>
2025-05-13 15:38:06 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,4 +1,4 @@
-//Tue May 13 2025 15:32:24 GMT+0000 (Coordinated Universal Time)
+//Tue May 13 2025 15:38:06 GMT+0000 (Coordinated Universal Time)
 //Base:<url id="cv1cref6o68qmpt26ol0" type="url" status="parsed" title="GitHub - echo094/decode-js: JS混淆代码的AST分析工具 AST analysis tool for obfuscated JS code" wc="2165">https://github.com/echo094/decode-js</url>
 //Modify:<url id="cv1cref6o68qmpt26olg" type="url" status="parsed" title="GitHub - smallfawn/decode_action: 世界上本来不存在加密，加密的人多了，也便成就了解密" wc="741">https://github.com/smallfawn/decode_action</url>
 const $ = new Env('福田e家');
@@ -88,6 +88,32 @@ async function main() {
       uid = login.data.uid;
       memberComplexCode = login.data.memberComplexCode;
       memberId = login.data.memberID;
+      let open = await commonPost(' /ehomes-new/homeManager/api/share/corsToActicity', {
+        "memberId": memberId,
+        "userId": uid,
+        "userType": "61",
+        "uid": uid,
+        "mobile": phone,
+        "tel": phone,
+        "phone": phone,
+        "brandName": "",
+        "seriesName": "",
+        "token": "ebf76685e48d4e14a9de6fccc76483e3",
+        "safeEnc": Date.now() - 2022020200,
+        "businessId": 1,
+        "activityNumber": "open",
+        "requestType": "0",
+        "type": "5",
+        "userNumber": memberId,
+        "channel": "1",
+        "name": "",
+        "remark": "打开APP"
+      });
+      if (open.code = 200) {
+        console.log('打开app成功');
+      } else {
+        console.log(`打开app：${open.msg}`);
+      }
       console.log('开始签到');
       if (login.data.signIn == "未签到") {
         let sign = await commonPost('/ehomes-new/homeManager/api/bonus/signActivity2nd', {
